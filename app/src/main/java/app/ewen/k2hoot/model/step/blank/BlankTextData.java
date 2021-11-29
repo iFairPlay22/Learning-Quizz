12b771431dfb435a8cb7e664b7ffa3c5e30e8dc9
Fabinours <44270057+Fabinours@users.noreply.github.com>
2021-11-29 17:53:09 +0100

    BlankText and GapSentence Layout

--- app/src/main/java/app/ewen/k2hoot/model/step/blank/BlankTextData.java
+++ app/src/main/java/app/ewen/k2hoot/model/step/blank/BlankTextData.java
@@ -1,6 +1,33 @@
 package app.ewen.k2hoot.model.step.blank;
 
+import java.util.List;
+
 import app.ewen.k2hoot.model.step.IStepData;
 
 public class BlankTextData implements IStepData {
+    //Phrase sous forme de liste de mots
+    private String[] words;
+    //Index dans la phrase des mots à compléter
+    private List<Integer> gapIndexes;
+
+    public BlankTextData(String[] words, List<Integer> gapIndexes) {
+        this.words = words;
+        this.gapIndexes = gapIndexes;
+    }
+
+    public String[] getWords() {
+        return words;
+    }
+
+    public void setWords(String[] words) {
+        this.words = words;
+    }
+
+    public List<Integer> getGapIndexes() {
+        return gapIndexes;
+    }
+
+    public void setGapIndexes(List<Integer> gapIndexes) {
+        this.gapIndexes = gapIndexes;
+    }
 }
--- app/src/main/java/app/ewen/k2hoot/model/step/blank/BlankTextStep.java
+++ app/src/main/java/app/ewen/k2hoot/model/step/blank/BlankTextStep.java
@@ -3,19 +3,42 @@ package app.ewen.k2hoot.model.step.blank;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import app.ewen.k2hoot.model.step.AbstractStep;
 
 public final class BlankTextStep extends AbstractStep<BlankTextData, BlankTextInput> {
 
     // GAME MANAGEMENT
+    //Phrase sous forme de liste de mots
+    private String[] mWords;
+    //Index dans la phrase des mots à compléter
+    private List<Integer> mGapIndexes;
+
+    private List<String> mAnswers;
+
+    public BlankTextStep(String[] words, List<Integer> gapIndexes) {
+        mWords = words;
+        mGapIndexes = gapIndexes;
+    }
+
     @Override
     public BlankTextData getData() {
-        return new BlankTextData();
+        return new BlankTextData(mWords,mGapIndexes);
     }
 
     @Override
     public boolean isGoodAnswer(BlankTextInput userInput) {
-        return false;
+        List<String> answers = userInput.getAnswers();
+        for (int i = 0; i < mGapIndexes.size(); i++) {
+            int posInSentence = mGapIndexes.get(i);
+            if(!mWords[posInSentence].equals(answers.get(i))){
+                return false;
+            }
+        }
+        return true;
     }
 
     // JSON
@@ -26,6 +49,9 @@ public final class BlankTextStep extends AbstractStep<BlankTextData, BlankTextIn
     // PARCELABLE
     protected BlankTextStep(Parcel in) {
         // ... = dest.writeString(...);
+        mWords = in.createStringArray();
+        mGapIndexes = in.readArrayList(Integer.class.getClassLoader());
+        mAnswers = in.createStringArrayList();
     }
 
     public static final Parcelable.Creator<BlankTextStep> CREATOR = new Parcelable.Creator<BlankTextStep>() {
@@ -43,5 +69,8 @@ public final class BlankTextStep extends AbstractStep<BlankTextData, BlankTextIn
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         // dest.writeString(...);
+        dest.writeStringArray(mWords);
+        dest.writeList(mGapIndexes);
+        dest.writeStringList(mAnswers);
     }
 }
